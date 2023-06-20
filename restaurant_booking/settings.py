040799fce496d67e9d7e0057a0740b045e0d31b1
DawKapi82 <dawid_kapi82@hotmail.com>
2023-06-20 12:29:22 +0000

    deployment to heroku

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-dawkapi82-restaurantboo-rm2ll7w33cm.ws-eu100.gitpod.io']
+ALLOWED_HOSTS = ['8000-dawkapi82-restaurantboo-rm2ll7w33cm.ws-eu100.gitpod.io', ' https://restaurant1-82632b2983ba.herokuapp.com']
 
 
 # Application definition
@@ -88,9 +88,18 @@ WSGI_APPLICATION = 'restaurant_booking.wsgi.application'
 # }
 
 
-DATABASES = {
-     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
- }
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
